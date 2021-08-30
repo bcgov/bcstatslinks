3b41d7d0cae4ecf1c441725ab3c8e8be82670943
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2021-08-30 10:33:30 -0700

    another attempt to fix examples

--- R/runExamples.R
+++ R/runExamples.R
@@ -1,32 +1,3 @@
-#' Run example shiny apps
-#' @param example either "simple" or "bcstatsheader"
-
-#' @export
-runExample <- function(example) {
-  # locate all the shiny app examples that exist
-  validExamples <- list.files(system.file("examples", package = "bcstatslinks"))
-
-  validExamplesMsg <-
-    paste0(
-      "Valid examples are: '",
-      paste(validExamples, collapse = "', '"),
-      "'")
-
-  # if an invalid example is given, throw an error
-  if (missing(example) || !nzchar(example) ||
-      !example %in% validExamples) {
-    stop(
-      'Please run `runExample()` with a valid example app as an argument.\n',
-      validExamplesMsg,
-      call. = FALSE)
-  }
-
-  # find and launch the app
-  appDir <- system.file("examples", example, package = "bcstatslinks")
-  shiny::runApp(appDir, display.mode = "normal")
-}
-
-
 #' See the code for an example shiny apps
 #' @param example either "simple" or "bcstatsheader"
 
